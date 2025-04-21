01790bfbfec30412f33b2b9fbdd04977a5e8b78c
Carlos Eduardo <carloseduardo391@hotmail.com>
2025-04-21 02:58:31 -0300

    primiero teste automatizado

--- cypress/e2e/3-loja -ebac/login.cy.js
+++ cypress/e2e/3-loja -ebac/login.cy.js
@@ -4,7 +4,12 @@ describe('funcionalidade: Login', () => {
 
     it('deve fazer login com sucesso', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        
+        cy.get('#username').type('eduardoteste@teste.com.br')
+        cy.get('#password').type('1234')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, eduardoteste (não é eduardoteste? Sair' )
+
     
     })
 })
